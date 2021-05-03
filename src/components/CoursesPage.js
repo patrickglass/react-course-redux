f48400eefcb9e08e5ebdff73e490150873bac334
Patrick Glass <patrickglass@gmail.com>
2021-05-02 22:28:32 -0700

    convert CoursesPage component from class to arrow function

--- src/components/CoursesPage.js
+++ src/components/CoursesPage.js
@@ -1,44 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { getCourses } from "../api/courseApi";
 
-class CoursesPage extends React.Component {
-  state = {
-    courses: [],
-  };
+const CoursesPage = () => {
+  const [courses, setCourses] = useState([]);
 
-  async componentDidMount() {
-    const resp = await getCourses();
-    this.setState({ courses: resp });
-  }
+  useEffect(() => {
+    const fetchData = async () => {
+      const resp = await getCourses();
+      setCourses(resp);
+    };
+    fetchData();
+  }, []);
 
-  render() {
-    return (
-      <>
-        <h2>Courses</h2>
-        <table className="table">
-          <thead>
-            <tr>
-              <th>Title</th>
-              <th>Author ID</th>
-              <th>Category</th>
-            </tr>
-          </thead>
-          <tbody>
-            {this.state.courses.map((course) => {
-              return (
-                <tr key={course.id}>
-                  <td>{course.title}</td>
-                  <td>{course.authorId}</td>
-                  <td>{course.category}</td>
-                </tr>
-              );
-            })}
-            <tr></tr>
-          </tbody>
-        </table>
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <h2>Courses</h2>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Author ID</th>
+            <th>Category</th>
+          </tr>
+        </thead>
+        <tbody>
+          {courses.map((course) => {
+            return (
+              <tr key={course.id}>
+                <td>{course.title}</td>
+                <td>{course.authorId}</td>
+                <td>{course.category}</td>
+              </tr>
+            );
+          })}
+          <tr></tr>
+        </tbody>
+      </table>
+    </>
+  );
+};
 
 export default CoursesPage;
