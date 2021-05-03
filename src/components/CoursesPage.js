e0dc9da2b988f6a7fc8d7f14961fd34e11b4067b
Patrick Glass <patrickglass@gmail.com>
2021-05-02 22:37:06 -0700

    refactor out CourseList

--- src/components/CoursesPage.js
+++ src/components/CoursesPage.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { getCourses } from "../api/courseApi";
+import CourseList from "./CourseList";
 
 const CoursesPage = () => {
   const [courses, setCourses] = useState([]);
@@ -15,27 +16,7 @@ const CoursesPage = () => {
   return (
     <>
       <h2>Courses</h2>
-      <table className="table">
-        <thead>
-          <tr>
-            <th>Title</th>
-            <th>Author ID</th>
-            <th>Category</th>
-          </tr>
-        </thead>
-        <tbody>
-          {courses.map((course) => {
-            return (
-              <tr key={course.id}>
-                <td>{course.title}</td>
-                <td>{course.authorId}</td>
-                <td>{course.category}</td>
-              </tr>
-            );
-          })}
-          <tr></tr>
-        </tbody>
-      </table>
+      <CourseList courses={courses} />
     </>
   );
 };
